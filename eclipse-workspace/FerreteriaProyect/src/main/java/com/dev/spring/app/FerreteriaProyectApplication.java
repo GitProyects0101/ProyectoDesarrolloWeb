0d80ae83fb389e5ac893eb706ec025ad30925615
Luis Estrada <GitProyects0101@gmail.com>
2019-09-19 21:24:56 -0500

    Versión inicial FerreteriaProyect
    Asignación de Tareas:
    
    FISTER HADINSON CASTILLO LUCAS
    * TB_PRODUCTO
    * TB_ABASTECIMIENTO
    
    LUIS ENRIQUE ESTRADA YOC
    *TB_PREORDEN
    *TB_FACTURA
    *TB_USUARIO
    
    KEILA MADAÍ HERRERA GUEVARA
    * TB_UNIDADMEDIDA
    * TB_FORMAPAGO
    
    DOMINGO VICTORIANO JUÁREZ FRANCISCO
    * TB_CREDITO
    * TB_DETALLE
    
    JUAN EDUARDO LÓPEZ GARCÍA
    * TB_ROL
    * TB_ASIGNAROL
    
    BRANDON QUENERIN RIVAS RIVAS
    * TB_PROVEEDOR
    * TB_CATEGORIA

---
+++ eclipse-workspace/FerreteriaProyect/src/main/java/com/dev/spring/app/FerreteriaProyectApplication.java
@@ -0,0 +1,13 @@
+package com.dev.spring.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerreteriaProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerreteriaProyectApplication.class, args);
+	}
+
+}
